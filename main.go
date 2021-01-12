d123fc267e9ccfd2250ee4489dfc9e39e073cac7
LakshyaNegi <lakshya.negi.1997@gmail.com>
2021-01-12 11:02:41 +0530

    starting with serve mux

--- main.go
+++ main.go
@@ -1,31 +1,18 @@
 package main
 
 import (
-	"fmt"
-	"io/ioutil"
 	"log"
 	"net/http"
+	"os"
 )
 
 func main() {
 
-	http.HandleFunc("/", func(rw http.ResponseWriter, r *http.Request) {
-		log.Println("Hello World")
-		d, err := ioutil.ReadAll(r.Body)
-		if err != nil {
-			http.Error(rw, "Oops", http.StatusBadRequest)
-			return
-		}
-		log.Printf("Data %s\n", d)
+	l := log.New(os.Stdout, "product-api", log.LstdFlags)
+	hh := handlers.NewHello(l)
 
-		fmt.Fprintf(rw, "Hello %s", d)
-	})
-
-	http.HandleFunc("/goodbye", func(rw http.ResponseWriter, r *http.Request) {
-		log.Println("Goodbye World")
-		d, _ := ioutil.ReadAll(r.Body)
-		log.Printf("Data %v", d)
-	})
+	sm := http.NewServeMux()
+	sm.Handle("/", hh)
 
 	http.ListenAndServe(":9090", nil)
 }
