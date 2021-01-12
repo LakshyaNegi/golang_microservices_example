e5b5bd6a7031d09d35aee025cb4d8aa16f03cd71
LakshyaNegi <lakshya.negi.1997@gmail.com>
2021-01-12 12:38:33 +0530

    starting with serve mux

--- handlers/goodbye.go
+++ handlers/goodbye.go
@@ -15,14 +15,4 @@ func NewGoodbye(l *log.Logger) *Goodbye {
 
 func (g *Goodbye) ServeHTTP(rw http.ResponseWriter, r *http.Request) {
 	rw.Write([]byte("Bye"))
-
-	//g.l.Println("Goodbye World")
-	// d, err := ioutil.ReadAll(r.Body)
-	// if err != nil {
-	// 	http.Error(rw, "Oops", http.StatusBadRequest)
-	// 	return
-	// }
-	// log.Printf("Data %s\n", d)
-
-	// fmt.Fprintf(rw, "Hello %s", d)
 }
